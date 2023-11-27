526dba09cbb0f5d0588a918a2607cf96b7141e81
tredsolei <dinhnhatbao2004@gmail.com>
2023-11-27 22:29:55 +0700

    khung#3

--- timetable/Form1.Designer.cs
+++ timetable/Form1.Designer.cs
@@ -31,14 +31,6 @@
             flowLayoutPanel1 = new FlowLayoutPanel();
             button1 = new Button();
             button2 = new Button();
-            groupBox1 = new GroupBox();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
-            label7 = new Label();
             panel1 = new Panel();
             panel2 = new Panel();
             panel3 = new Panel();
@@ -81,8 +73,14 @@
             panel40 = new Panel();
             panel41 = new Panel();
             panel42 = new Panel();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             flowLayoutPanel1.SuspendLayout();
-            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -129,14 +127,14 @@
             flowLayoutPanel1.Controls.Add(panel40);
             flowLayoutPanel1.Controls.Add(panel41);
             flowLayoutPanel1.Controls.Add(panel42);
-            flowLayoutPanel1.Location = new Point(12, 109);
+            flowLayoutPanel1.Location = new Point(12, 146);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
             flowLayoutPanel1.Size = new Size(1538, 804);
             flowLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(1240, 942);
+            button1.Location = new Point(1240, 960);
             button1.Name = "button1";
             button1.Size = new Size(150, 50);
             button1.TabIndex = 1;
@@ -145,90 +143,12 @@
             // 
             // button2
             // 
-            button2.Location = new Point(1400, 942);
+            button2.Location = new Point(1400, 960);
             button2.Name = "button2";
             button2.Size = new Size(150, 50);
             button2.TabIndex = 2;
             button2.Text = "Sau";
             button2.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            groupBox1.Controls.Add(label7);
-            groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(label5);
-            groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(label2);
-            groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(12, 27);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(1538, 76);
-            groupBox1.TabIndex = 3;
-            groupBox1.TabStop = false;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(71, 35);
-            label1.Name = "label1";
-            label1.Size = new Size(97, 32);
-            label1.TabIndex = 0;
-            label1.Text = "Thứ Hai";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(291, 35);
-            label2.Name = "label2";
-            label2.Size = new Size(88, 32);
-            label2.TabIndex = 1;
-            label2.Text = "Thứ Ba";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(506, 35);
-            label3.Name = "label3";
-            label3.Size = new Size(89, 32);
-            label3.TabIndex = 2;
-            label3.Text = "Thứ Tư";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(714, 35);
-            label4.Name = "label4";
-            label4.Size = new Size(113, 32);
-            label4.TabIndex = 3;
-            label4.Text = "Thứ Năm";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(935, 35);
-            label5.Name = "label5";
-            label5.Size = new Size(101, 32);
-            label5.TabIndex = 4;
-            label5.Text = "Thứ Sáu";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(1149, 35);
-            label6.Name = "label6";
-            label6.Size = new Size(100, 32);
-            label6.TabIndex = 5;
-            label6.Text = "Thứ Bảy";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(1364, 35);
-            label7.Name = "label7";
-            label7.Size = new Size(116, 32);
-            label7.TabIndex = 6;
-            label7.Text = "Chủ Nhật";
             // 
             // panel1
             // 
@@ -524,12 +444,88 @@
             panel42.Size = new Size(213, 128);
             panel42.TabIndex = 41;
             // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label7.Location = new Point(1342, 94);
+            label7.Name = "label7";
+            label7.Size = new Size(185, 44);
+            label7.TabIndex = 13;
+            label7.Text = "Chủ Nhật";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label6.Location = new Point(1133, 94);
+            label6.Name = "label6";
+            label6.Size = new Size(170, 44);
+            label6.TabIndex = 12;
+            label6.Text = "Thứ Bảy";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label5.Location = new Point(911, 94);
+            label5.Name = "label5";
+            label5.Size = new Size(171, 44);
+            label5.TabIndex = 11;
+            label5.Text = "Thứ Sáu";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label4.Location = new Point(687, 94);
+            label4.Name = "label4";
+            label4.Size = new Size(183, 44);
+            label4.TabIndex = 10;
+            label4.Text = "Thứ Năm";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label3.Location = new Point(486, 94);
+            label3.Name = "label3";
+            label3.Size = new Size(152, 44);
+            label3.TabIndex = 9;
+            label3.Text = "Thứ Tư";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label2.Location = new Point(266, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(150, 44);
+            label2.TabIndex = 8;
+            label2.Text = "Thứ Ba";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 13.875F, FontStyle.Bold);
+            label1.Location = new Point(42, 94);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 44);
+            label1.TabIndex = 7;
+            label1.Text = "Thứ Hai";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1562, 1023);
-            Controls.Add(groupBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(flowLayoutPanel1);
@@ -538,9 +534,8 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "timetable";
             flowLayoutPanel1.ResumeLayout(false);
-            groupBox1.ResumeLayout(false);
-            groupBox1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -584,7 +579,12 @@
         private Panel panel34;
         private Panel panel35;
         private Panel panel36;
-        private GroupBox groupBox1;
+        private Panel panel37;
+        private Panel panel38;
+        private Panel panel39;
+        private Panel panel40;
+        private Panel panel41;
+        private Panel panel42;
         private Label label7;
         private Label label6;
         private Label label5;
@@ -592,11 +592,5 @@
         private Label label3;
         private Label label2;
         private Label label1;
-        private Panel panel37;
-        private Panel panel38;
-        private Panel panel39;
-        private Panel panel40;
-        private Panel panel41;
-        private Panel panel42;
     }
 }
