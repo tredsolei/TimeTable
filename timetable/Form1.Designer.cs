681aed344580a372fbbe4995ed619538d3ad6e6c
tredsolei <dinhnhatbao2004@gmail.com>
2023-11-27 22:21:46 +0700

    khung

--- timetable/Form1.Designer.cs
+++ timetable/Form1.Designer.cs
@@ -28,12 +28,515 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            panel8 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            panel12 = new Panel();
+            panel13 = new Panel();
+            panel14 = new Panel();
+            panel15 = new Panel();
+            panel16 = new Panel();
+            panel17 = new Panel();
+            panel18 = new Panel();
+            panel19 = new Panel();
+            panel20 = new Panel();
+            panel21 = new Panel();
+            panel22 = new Panel();
+            panel23 = new Panel();
+            panel24 = new Panel();
+            panel25 = new Panel();
+            panel26 = new Panel();
+            panel27 = new Panel();
+            panel28 = new Panel();
+            panel29 = new Panel();
+            panel30 = new Panel();
+            panel31 = new Panel();
+            panel32 = new Panel();
+            panel33 = new Panel();
+            panel34 = new Panel();
+            panel35 = new Panel();
+            panel36 = new Panel();
+            flowLayoutPanel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(panel4);
+            flowLayoutPanel1.Controls.Add(panel5);
+            flowLayoutPanel1.Controls.Add(panel6);
+            flowLayoutPanel1.Controls.Add(panel7);
+            flowLayoutPanel1.Controls.Add(panel8);
+            flowLayoutPanel1.Controls.Add(panel9);
+            flowLayoutPanel1.Controls.Add(panel10);
+            flowLayoutPanel1.Controls.Add(panel11);
+            flowLayoutPanel1.Controls.Add(panel12);
+            flowLayoutPanel1.Controls.Add(panel13);
+            flowLayoutPanel1.Controls.Add(panel14);
+            flowLayoutPanel1.Controls.Add(panel15);
+            flowLayoutPanel1.Controls.Add(panel16);
+            flowLayoutPanel1.Controls.Add(panel17);
+            flowLayoutPanel1.Controls.Add(panel18);
+            flowLayoutPanel1.Controls.Add(panel19);
+            flowLayoutPanel1.Controls.Add(panel20);
+            flowLayoutPanel1.Controls.Add(panel21);
+            flowLayoutPanel1.Controls.Add(panel22);
+            flowLayoutPanel1.Controls.Add(panel23);
+            flowLayoutPanel1.Controls.Add(panel24);
+            flowLayoutPanel1.Controls.Add(panel25);
+            flowLayoutPanel1.Controls.Add(panel26);
+            flowLayoutPanel1.Controls.Add(panel27);
+            flowLayoutPanel1.Controls.Add(panel28);
+            flowLayoutPanel1.Controls.Add(panel29);
+            flowLayoutPanel1.Controls.Add(panel30);
+            flowLayoutPanel1.Controls.Add(panel31);
+            flowLayoutPanel1.Controls.Add(panel32);
+            flowLayoutPanel1.Controls.Add(panel33);
+            flowLayoutPanel1.Controls.Add(panel34);
+            flowLayoutPanel1.Controls.Add(panel35);
+            flowLayoutPanel1.Controls.Add(panel36);
+            flowLayoutPanel1.Location = new Point(12, 109);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1538, 804);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1240, 942);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 50);
+            button1.TabIndex = 1;
+            button1.Text = "Trước";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1400, 942);
+            button2.Name = "button2";
+            button2.Size = new Size(150, 50);
+            button2.TabIndex = 2;
+            button2.Text = "Sau";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 27);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1538, 76);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(61, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Thứ Hai";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(265, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Thứ Ba";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(489, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Thứ Tư";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(696, 25);
+            label4.Name = "label4";
+            label4.Size = new Size(113, 32);
+            label4.TabIndex = 3;
+            label4.Text = "Thứ Năm";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(919, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 32);
+            label5.TabIndex = 4;
+            label5.Text = "Thứ Sáu";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1144, 25);
+            label6.Name = "label6";
+            label6.Size = new Size(100, 32);
+            label6.TabIndex = 5;
+            label6.Text = "Thứ Bảy";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(1375, 25);
+            label7.Name = "label7";
+            label7.Size = new Size(116, 32);
+            label7.TabIndex = 6;
+            label7.Text = "Chủ Nhật";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(3, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 128);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(259, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 128);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(515, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(250, 128);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(771, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(250, 128);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.Location = new Point(1027, 3);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(250, 128);
+            panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.Location = new Point(1283, 3);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(250, 128);
+            panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            panel7.Location = new Point(3, 137);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(250, 128);
+            panel7.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            panel8.Location = new Point(259, 137);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(250, 128);
+            panel8.TabIndex = 7;
+            // 
+            // panel9
+            // 
+            panel9.Location = new Point(515, 137);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(250, 128);
+            panel9.TabIndex = 8;
+            // 
+            // panel10
+            // 
+            panel10.Location = new Point(771, 137);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(250, 128);
+            panel10.TabIndex = 9;
+            // 
+            // panel11
+            // 
+            panel11.Location = new Point(1027, 137);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(250, 128);
+            panel11.TabIndex = 10;
+            // 
+            // panel12
+            // 
+            panel12.Location = new Point(1283, 137);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(250, 128);
+            panel12.TabIndex = 11;
+            // 
+            // panel13
+            // 
+            panel13.Location = new Point(3, 271);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(250, 128);
+            panel13.TabIndex = 12;
+            // 
+            // panel14
+            // 
+            panel14.Location = new Point(259, 271);
+            panel14.Name = "panel14";
+            panel14.Size = new Size(250, 128);
+            panel14.TabIndex = 13;
+            // 
+            // panel15
+            // 
+            panel15.Location = new Point(515, 271);
+            panel15.Name = "panel15";
+            panel15.Size = new Size(250, 128);
+            panel15.TabIndex = 14;
+            // 
+            // panel16
+            // 
+            panel16.Location = new Point(771, 271);
+            panel16.Name = "panel16";
+            panel16.Size = new Size(250, 128);
+            panel16.TabIndex = 15;
+            // 
+            // panel17
+            // 
+            panel17.Location = new Point(1027, 271);
+            panel17.Name = "panel17";
+            panel17.Size = new Size(250, 128);
+            panel17.TabIndex = 16;
+            // 
+            // panel18
+            // 
+            panel18.Location = new Point(1283, 271);
+            panel18.Name = "panel18";
+            panel18.Size = new Size(250, 128);
+            panel18.TabIndex = 17;
+            // 
+            // panel19
+            // 
+            panel19.Location = new Point(3, 405);
+            panel19.Name = "panel19";
+            panel19.Size = new Size(250, 128);
+            panel19.TabIndex = 18;
+            // 
+            // panel20
+            // 
+            panel20.Location = new Point(259, 405);
+            panel20.Name = "panel20";
+            panel20.Size = new Size(250, 128);
+            panel20.TabIndex = 19;
+            // 
+            // panel21
+            // 
+            panel21.Location = new Point(515, 405);
+            panel21.Name = "panel21";
+            panel21.Size = new Size(250, 128);
+            panel21.TabIndex = 20;
+            // 
+            // panel22
+            // 
+            panel22.Location = new Point(771, 405);
+            panel22.Name = "panel22";
+            panel22.Size = new Size(250, 128);
+            panel22.TabIndex = 21;
+            // 
+            // panel23
+            // 
+            panel23.Location = new Point(1027, 405);
+            panel23.Name = "panel23";
+            panel23.Size = new Size(250, 128);
+            panel23.TabIndex = 22;
+            // 
+            // panel24
+            // 
+            panel24.Location = new Point(1283, 405);
+            panel24.Name = "panel24";
+            panel24.Size = new Size(250, 128);
+            panel24.TabIndex = 23;
+            // 
+            // panel25
+            // 
+            panel25.Location = new Point(3, 539);
+            panel25.Name = "panel25";
+            panel25.Size = new Size(250, 128);
+            panel25.TabIndex = 24;
+            // 
+            // panel26
+            // 
+            panel26.Location = new Point(259, 539);
+            panel26.Name = "panel26";
+            panel26.Size = new Size(250, 128);
+            panel26.TabIndex = 25;
+            // 
+            // panel27
+            // 
+            panel27.Location = new Point(515, 539);
+            panel27.Name = "panel27";
+            panel27.Size = new Size(250, 128);
+            panel27.TabIndex = 26;
+            // 
+            // panel28
+            // 
+            panel28.Location = new Point(771, 539);
+            panel28.Name = "panel28";
+            panel28.Size = new Size(250, 128);
+            panel28.TabIndex = 27;
+            // 
+            // panel29
+            // 
+            panel29.Location = new Point(1027, 539);
+            panel29.Name = "panel29";
+            panel29.Size = new Size(250, 128);
+            panel29.TabIndex = 28;
+            // 
+            // panel30
+            // 
+            panel30.Location = new Point(1283, 539);
+            panel30.Name = "panel30";
+            panel30.Size = new Size(250, 128);
+            panel30.TabIndex = 29;
+            // 
+            // panel31
+            // 
+            panel31.Location = new Point(3, 673);
+            panel31.Name = "panel31";
+            panel31.Size = new Size(250, 128);
+            panel31.TabIndex = 30;
+            // 
+            // panel32
+            // 
+            panel32.Location = new Point(259, 673);
+            panel32.Name = "panel32";
+            panel32.Size = new Size(250, 128);
+            panel32.TabIndex = 31;
+            // 
+            // panel33
+            // 
+            panel33.Location = new Point(515, 673);
+            panel33.Name = "panel33";
+            panel33.Size = new Size(250, 128);
+            panel33.TabIndex = 32;
+            // 
+            // panel34
+            // 
+            panel34.Location = new Point(771, 673);
+            panel34.Name = "panel34";
+            panel34.Size = new Size(250, 128);
+            panel34.TabIndex = 33;
+            // 
+            // panel35
+            // 
+            panel35.Location = new Point(1027, 673);
+            panel35.Name = "panel35";
+            panel35.Size = new Size(250, 128);
+            panel35.TabIndex = 34;
+            // 
+            // panel36
+            // 
+            panel36.Location = new Point(1283, 673);
+            panel36.Name = "panel36";
+            panel36.Size = new Size(250, 128);
+            panel36.TabIndex = 35;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1562, 1023);
+            Controls.Add(groupBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(flowLayoutPanel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "timetable";
+            flowLayoutPanel1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel9;
+        private Panel panel10;
+        private Panel panel11;
+        private Panel panel12;
+        private Panel panel13;
+        private Panel panel14;
+        private Panel panel15;
+        private Panel panel16;
+        private Panel panel17;
+        private Panel panel18;
+        private Panel panel19;
+        private Panel panel20;
+        private Panel panel21;
+        private Panel panel22;
+        private Panel panel23;
+        private Panel panel24;
+        private Panel panel25;
+        private Panel panel26;
+        private Panel panel27;
+        private Panel panel28;
+        private Panel panel29;
+        private Panel panel30;
+        private Panel panel31;
+        private Panel panel32;
+        private Panel panel33;
+        private Panel panel34;
+        private Panel panel35;
+        private Panel panel36;
+        private GroupBox groupBox1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
